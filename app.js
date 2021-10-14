01175f47a4ac3dd4936df8785f430252f973d387
kmcabano <kmcabano@gmail.com>
2021-10-14 17:04:51 -0400

    start psuedocode

---
+++ app.js
@@ -0,0 +1,15 @@
+// 1. Define & store necessary variables
+
+// 2. Cache element references, this will be especially necessary for using the DOM
+
+// 3. Startup
+  // 3.1 Initialize function
+  // 3.2 Render function
+  // 3.3 Include event listener(s)
+
+// 4. Define & store necessary constants
+  // 4.1. for the sake of organization, store clues and answers in a separate js file
+
+// 5. Handle a player selecting a clue
+
+// X. Create event listener for light/dark toggle
