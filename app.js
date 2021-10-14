cf0e09ead5f87bb875cae1985e1561981c5fceea
kmcabano <kmcabano@gmail.com>
2021-10-14 19:19:25 -0400

    psuedocode WIP

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 // 1. Define & store necessary variables
+  // 1.1 The game board will be an array
+    // 1.1.1 The category squares will be defined with their titles, the answer squares with their dollar values
+
+  // 1.2 Have a turn variable
+    // 1.2.1 While J! usually has 3 players, I think I will just do two
+    
+  // 1.3.
 
 // 2. Cache element references, this will be especially necessary for using the DOM
 
@@ -8,7 +15,7 @@
   // 3.3 Include event listener(s)
 
 // 4. Define & store necessary constants
-  // 4.1. for the sake of organization, store clues and answers in a separate js file
+  // 4.1 For the sake of organization, store clues and answers in a separate js file, similar to the boostrap code-along exercise
 
 // 5. Handle a player selecting a clue
 
