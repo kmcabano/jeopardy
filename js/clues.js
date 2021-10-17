a0aa695e276554ef0d86aff0a37e686e2b81d130
kmcabano <kmcabano@gmail.com>
2021-10-17 15:25:44 -0700

    organization, first categoty data entered

---
+++ js/clues.js
@@ -0,0 +1,47 @@
+// Category 1: "RU" Serious
+const firstCat = [
+  { value: '$200',
+  clue: `It's the monetary unit of Russia & of Belarus`,
+  response: `What is the ruble?`,
+  wrongOne: ``,
+  wrongTwo: ``},
+  {value: '$400',
+  clue: `Todd Gurley for the Falcons, or Ezekiel Elliott for the Cowboys`,
+  response: `What is running back?`,
+  wrongOne: ``,
+  wrongTwo: ``},
+  {value: '$600',
+  clue: `It's the opposite of urban`,
+  response: `What is rural?`,
+  wrongOne: ``,
+  wrongTwo: ``},
+  {value: '$800',
+  clue: `This 6-letter grouse gets its name from its collar of neck feathers displayed by the male`,
+  response: `What is ruffed?`,
+  wrongOne: ``,
+  wrongTwo: ``},
+  {value: '$1000',
+  clue: `This root vegetable is believed to be a cross between a cabbage & a turnip`,
+  response: `What is a rutabaga?`,
+  wrongOne: ``,
+  wrongTwo: ``}
+]
+
+const secondCat = [
+
+]
+const thirdCat = [
+
+]
+const fourthCat = [
+
+]
+const fifthCat = [
+
+]
+const sixthCat = [
+
+]
+
+
+
