7cf1ba1dd355519cd694bb385f961f31689fc3b2
kmcabano <kmcabano@gmail.com>
2021-10-17 19:46:04 -0700

    cleanup click function

--- js/app.js
+++ js/app.js
@@ -31,42 +31,30 @@ function render() {
 function handleClick(e) {
   let catEl = [catA, catB, catC, catD, catE, catF]
 	const clickedIdx = e.target.id.slice(2)
-  if (clickedIdx > 5 && clickedIdx < 12) {
-    var clickedVal = 0
-  }
-  if (clickedIdx > 11 && clickedIdx < 18) {
-    var clickedVal = 1
-  }
-  if (clickedIdx > 17 && clickedIdx < 24) {
-    var clickedVal = 2
-  }
-  if (clickedIdx > 23 && clickedIdx < 30) {
-    var clickedVal = 3
-  }
-  if (clickedIdx > 29 && clickedIdx < 36) {
-    var clickedVal = 4
-  }
-  if (e.target.className.substring(8) === `one`) {
-    var clickedCat = 0
-  }
-  if (e.target.className.substring(8) === `two`) {
-    var clickedCat = 1
-  }
-  if (e.target.className.substring(8) === `three`) {
-    var clickedCat = 2
-  }
-  if (e.target.className.substring(8) === `four`) {
-    var clickedCat = 3
-  }
-  if (e.target.className.substring(8) === `five`) {
-    var clickedCat = 4
-  }
-  if (e.target.className.substring(8) === `six`) {
-    var clickedCat = 5
-  }
+  let clickedVal =
+    clickedIdx > 5 && clickedIdx < 12
+    ? 0
+    : clickedIdx > 11 && clickedIdx < 18
+    ? 1
+    : clickedIdx > 17 && clickedIdx < 24
+    ? 2
+    : clickedIdx > 23 && clickedIdx < 30
+    ? 3
+    : 4
+  let clickedCat =
+    e.target.className.substring(8) === `one`
+    ? 0
+    : e.target.className.substring(8) === `two`
+    ? 1
+    : e.target.className.substring(8) === `three`
+    ? 2
+    : e.target.className.substring(8) === `four`
+    ? 3
+    : e.target.className.substring(8) === `five`
+    ? 4
+    : 5
   if (clickedIdx > 5) {
     if (e.target.classList.contains('clicked')) {
-      clueEl.innerText = null
       return
     }
     if (boardSq[clickedIdx] !== null) {
