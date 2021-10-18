f0779c2a070d609f5894133b0cc6f907c5e3ea73
kmcabano <kmcabano@gmail.com>
2021-10-17 17:32:06 -0700

    add categories 2, 3

--- js/clues.js
+++ js/clues.js
@@ -1,46 +1,175 @@
-// Category 1: "RU" Serious
-const firstCat = [
+// Category 1:
+// Eyes
+// Show #2053 - Wednesday, July 7, 1993
+const catA = [
+  { value: '$200',
+  clue: `The color of this ring around the pupil is inherited`,
+  response: `What is iris?`,
+  wrongOne: `What is pupil`,
+  wrongTwo: `What is cornea`},
+  {value: '$400',
+  clue: `In this disease increased fluid pressure in the eye damages the optic nerve`,
+  response: `What is glaucoma?`,
+  wrongOne: `Wrong`,
+  wrongTwo: `Wrong`},
+  {value: '$600',
+  clue: `This vitamin is needed to produce visual purple, a pigment allowing one to see in dim light`,
+  response: `What is vitamin A?`,
+  wrongOne: `What is vitamin C?`,
+  wrongTwo: `What is vitamin D?`},
+  {value: '$800',
+  clue: `This common disorder is an infection of the small glands of the eyelashes`,
+  response: `What is stye?`,
+  wrongOne: `What is conjunctivitis?`,
+  wrongTwo: `What is pink eye?`},
+  {value: '$1000',
+  clue: `This jelly-like "humor" occupies about 10% of the eyeball`,
+  response: `What is vitreous humor?`,
+  wrongOne: `Wrong`,
+  wrongTwo: `Wrong`}
+]
+
+// Category 2:
+// "Born In Connecticut"
+// Show #7341 - Monday, July 11, 2016
+const catB = [
+  { value: '$200',
+  clue: `A book called "Homegrown Terror" chronicles this patriot-turned-traitor & the burning of New London`,
+  response: `Who is Benedict Arnold?`,
+  wrongOne: `Wrong`,
+  wrongTwo: `Wrong`},
+  {value: '$400',
+  clue: `Before he ran off & ran the circus, he published a Danbury newspaper & was arrested for libel`,
+  response: `Who is P.T. Barnum?`,
+  wrongOne: `Wrong`,
+  wrongTwo: `Wrong`},
+  {value: '$600',
+  clue: `This Yale grad put out "An American Dictionary of the English Language" in 1828, when he was 70`,
+  response: `Who is Noah Webster?`,
+  wrongOne: `Wrong`,
+  wrongTwo: `Wrong`},
+  {value: '$800',
+  clue: `In 1868 a church in Hartford, Connecticut was founded in memory of this revolver inventor & some of his children`,
+  response: `Who is Samuel Colt?`,
+  wrongOne: `Wrong`,
+  wrongTwo: `Wrong`},
+  {value: '$1000',
+  clue: `The last name of this Connecticut-born jeweler is seen on coveted blue boxes`,
+  response: `What is Tiffany?`,
+  wrongOne: `Wrong`,
+  wrongTwo: `Wrong`}
+]
+
+// Category 3:
+// "RU" Serious
+// Show #8329 - Thursday, February 4, 2021
+const catC = [
   { value: '$200',
   clue: `It's the monetary unit of Russia & of Belarus`,
   response: `What is the ruble?`,
-  wrongOne: ``,
-  wrongTwo: ``},
+  wrongOne: `Wrong`,
+  wrongTwo: `Wrong`},
   {value: '$400',
   clue: `Todd Gurley for the Falcons, or Ezekiel Elliott for the Cowboys`,
   response: `What is running back?`,
-  wrongOne: ``,
-  wrongTwo: ``},
+  wrongOne: `Wrong`,
+  wrongTwo: `Wrong`},
   {value: '$600',
   clue: `It's the opposite of urban`,
   response: `What is rural?`,
-  wrongOne: ``,
-  wrongTwo: ``},
+  wrongOne: `Wrong`,
+  wrongTwo: `Wrong`},
   {value: '$800',
   clue: `This 6-letter grouse gets its name from its collar of neck feathers displayed by the male`,
   response: `What is ruffed?`,
-  wrongOne: ``,
-  wrongTwo: ``},
+  wrongOne: `Wrong`,
+  wrongTwo: `Wrong`},
   {value: '$1000',
   clue: `This root vegetable is believed to be a cross between a cabbage & a turnip`,
   response: `What is a rutabaga?`,
+  wrongOne: `Wrong`,
+  wrongTwo: `Wrong`}
+]
+const catD = [
+  { value: '$200',
+  clue: ``,
+  response: ``,
+  wrongOne: ``,
+  wrongTwo: ``},
+  {value: '$400',
+  clue: ``,
+  response: ``,
+  wrongOne: ``,
+  wrongTwo: ``},
+  {value: '$600',
+  clue: ``,
+  response: ``,
+  wrongOne: ``,
+  wrongTwo: ``},
+  {value: '$800',
+  clue: ``,
+  response: ``,
+  wrongOne: ``,
+  wrongTwo: ``},
+  {value: '$1000',
+  clue: ``,
+  response: ``,
   wrongOne: ``,
   wrongTwo: ``}
 ]
-
-const secondCat = [
-
+const catE = [
+  { value: '$200',
+  clue: ``,
+  response: ``,
+  wrongOne: ``,
+  wrongTwo: ``},
+  {value: '$400',
+  clue: ``,
+  response: ``,
+  wrongOne: ``,
+  wrongTwo: ``},
+  {value: '$600',
+  clue: ``,
+  response: ``,
+  wrongOne: ``,
+  wrongTwo: ``},
+  {value: '$800',
+  clue: ``,
+  response: ``,
+  wrongOne: ``,
+  wrongTwo: ``},
+  {value: '$1000',
+  clue: ``,
+  response: ``,
+  wrongOne: ``,
+  wrongTwo: ``}
 ]
-const thirdCat = [
-
-]
-const fourthCat = [
-
-]
-const fifthCat = [
-
-]
-const sixthCat = [
-
+const catF = [
+  { value: '$200',
+  clue: ``,
+  response: ``,
+  wrongOne: ``,
+  wrongTwo: ``},
+  {value: '$400',
+  clue: ``,
+  response: ``,
+  wrongOne: ``,
+  wrongTwo: ``},
+  {value: '$600',
+  clue: ``,
+  response: ``,
+  wrongOne: ``,
+  wrongTwo: ``},
+  {value: '$800',
+  clue: ``,
+  response: ``,
+  wrongOne: ``,
+  wrongTwo: ``},
+  {value: '$1000',
+  clue: ``,
+  response: ``,
+  wrongOne: ``,
+  wrongTwo: ``}
 ]
 
 
