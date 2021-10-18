08fd9b3aaa49602f480f06e72e00615451ac559f
kmcabano <kmcabano@gmail.com>
2021-10-17 19:12:53 -0700

    clicking square now accesses correct clue

--- js/app.js
+++ js/app.js
@@ -28,43 +28,43 @@ function handleClick(e) {
   let catEl = [catA, catB, catC, catD, catE, catF]
 	const clickedIdx = e.target.id.slice(2)
   if (clickedIdx > 5 && clickedIdx < 12) {
-    let clickedVal = 0
+    var clickedVal = 0
   }
   if (clickedIdx > 11 && clickedIdx < 18) {
-    let clickedVal = 1
+    var clickedVal = 1
   }
   if (clickedIdx > 17 && clickedIdx < 24) {
-    let clickedVal = 2
+    var clickedVal = 2
   }
   if (clickedIdx > 23 && clickedIdx < 30) {
-    let clickedVal = 3
+    var clickedVal = 3
   }
   if (clickedIdx > 29 && clickedIdx < 36) {
-    let clickedVal = 4
+    var clickedVal = 4
   }
   if (e.target.className.substring(8) === `one`) {
-    let clickedCat = 0
+    var clickedCat = 0
   }
   if (e.target.className.substring(8) === `two`) {
-    let clickedCat = 1
+    var clickedCat = 1
   }
   if (e.target.className.substring(8) === `three`) {
-    let clickedCat = 2
+    var clickedCat = 2
   }
   if (e.target.className.substring(8) === `four`) {
-    let clickedCat = 3
+    var clickedCat = 3
   }
   if (e.target.className.substring(8) === `five`) {
-    let clickedCat = 4
+    var clickedCat = 4
   }
   if (e.target.className.substring(8) === `six`) {
-    let clickedCat = 5
+    var clickedCat = 5
   }
   if (clickedIdx > 5) {
     if (boardSq[clickedIdx] !== null) {
-      boardSq[clickedIdx].innerText = ''
-    } 
+      boardSq[clickedIdx].innerText = null
+      clueEl.innerText = `${(catEl[clickedCat][clickedVal].clue)}`
+    }
   } 
-
 render();
 }
